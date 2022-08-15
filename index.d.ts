12bd8de7407acb07045aad15ef84f5875b214eba
zjcqoo <zjcqoo@gmail.com>
2022-08-15 10:47:50 +0800

    style: use strict mode, update type, doc

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-interface str2gbkOpt<T extends Uint8Array> {
+export interface str2gbkOpt<T extends Uint8Array = Uint8Array> {
   onAlloc?: (len: number) => T,
   onError?: (index: number, input: string) => number,
 }
